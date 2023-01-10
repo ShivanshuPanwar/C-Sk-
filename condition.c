ffea3571a61fe7a89aea1fc789fcd62f77c0425a
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-01-10 23:54:30 +0530

    Add files via upload

---
+++ condition.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+int main()
+{
+    int num1,num2;
+    
+    printf("Enter number1");
+    scanf("%d, &num1:");
+
+    printf("Enter number2");
+    scanf("%d, &num2:");
+
+    (num1>num2)? printf("Number 1 is greater"): printf("Number 2 is geater");
+
+    return 0;
+}
